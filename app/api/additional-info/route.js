3cbd5dac9fe3b78f2a204c7beb91600a6f95f946
joshuabriones <joshua.briones@cit.edu>
2024-08-01 04:53:36 +0800

    updates

--- app/api/additional-info/route.js
+++ app/api/additional-info/route.js
@@ -2,36 +2,44 @@ import { NextResponse } from "next/server";
 import db from "@/lib/db";
 
 export async function POST(request) {
-  const { userId, role, firstName, lastName, image } = await request.json();
+  try {
+    const { userId, role, firstName, lastName, image } = await request.json();
 
-  console.log(userId, role, firstName, lastName);
+    console.log(userId, role, firstName, lastName);
 
-  if (!userId || !role) {
-    return NextResponse.json({
-      status: 400,
-      message: "All fields are required",
-    });
-  }
+    if (!userId || !role || !firstName || !lastName) {
+      return NextResponse.json({
+        status: 400,
+        message: "All fields are required",
+      });
+    }
 
-  const updatedUser = await db.user.update({
-    where: { id: userId },
-    data: {
-      role: role,
-      profile: {
-        create: {
-          firstName: firstName,
-          lastName: lastName,
-          image:
-            image ||
-            `https://ui-avatars.com/api/?name=${firstName}+${lastName}`,
+    const updatedUser = await db.user.update({
+      where: { id: userId },
+      data: {
+        role: role,
+        profile: {
+          create: {
+            firstName: firstName,
+            lastName: lastName,
+            image:
+              image ||
+              `https://ui-avatars.com/api/?name=${firstName}+${lastName}`,
+          },
         },
       },
-    },
-  });
+    });
 
-  return NextResponse.json({
-    user: updatedUser,
-    message: "User profile updated successfully",
-    redirectUrl: "/login",
-  });
+    return NextResponse.json({
+      user: updatedUser,
+      message: "User profile updated successfully",
+      redirectUrl: "/login",
+    });
+  } catch (error) {
+    console.error("Error updating user profile:", error);
+    return NextResponse.json({
+      status: 500,
+      message: "Internal server error",
+    });
+  }
 }
